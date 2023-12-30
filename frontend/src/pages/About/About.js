c5a2303fdc69c61a180b14dfb8b4cf36a4ec909e
Younes <youness.idbouichou@gmail.com>
2023-12-30 15:28:03 +0100

    Initial Commit

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">About</h1>
+            <div className="about-main">
+                <p className="about-content">
+                    This is an realtime AI based Yoga Trainer which detects your pose how well you are doing.
+                    
+                </p>
+                
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/pages/Tutorials/Tutorials.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './Tutorials.css'
+
+import { tutorials, fixCamera } from '../../utils/data'
+
+export default function Tutorials() {
+    return (
+        <div className="tutorials-container">
+            <h1 className="tutorials-heading">Basic </h1>
+            <div className="tutorials-content-container">
+                {tutorials.map((tutorial) => (
+                    <p className="tutorials-content">{tutorial}</p>
+                ))}
+            </div>
+            <h1 className="tutorials-heading">Experiencing issues with your camera?</h1>
+            <div className="tutorials-content-container">
+                {fixCamera.map((points) => (
+                    <p className="tutorials-content">{points}</p>
+                ))}
+            </div>
+            
+        </div>
+    )
+}
